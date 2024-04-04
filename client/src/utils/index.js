66b747be1dd494ab6877354b050e8be051d4dfde
JAYANT SINGH <97082996+jayantsB123@users.noreply.github.com>
2024-04-05 00:03:29 +0530

    Update index.js

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,7 +2,8 @@ import FileSaver from 'file-saver';
 import { surpriseMePrompts } from '../constant';
 
 
-export function getRandomPrompt(prompt) {
+export function getRandomPrompt(prompt) 
+{
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts[randomIndex];
 
@@ -11,6 +12,7 @@ export function getRandomPrompt(prompt) {
   return randomPrompt;
 }
 
-export async function downloadImage(_id, photo) {
+export async function downloadImage(_id, photo) 
+{
   FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
